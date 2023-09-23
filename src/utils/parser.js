b955ea215682184b2d8c9f54c2af411975e5658c
Karthik <karthikajar555@gmail.com>
2023-09-23 21:37:12 +0530

    Fixed - array of objects, array of arrays - ToFix - Prevent unnecessary/duplicate edges

--- src/utils/parser.js
+++ src/utils/parser.js
@@ -41,101 +41,173 @@ function getPrimitives(obj) {
     return { values, children };
   }
 
-  export function generateNodesAndEdges(obj, parent, nodes = [], edges = []) {
+  const EDGE_TYPE = 'simplebezier'
+  const NODE_TYPE = 'customNode'
+
+  // It is a hotfix. Need to find the root cause
+  // why duplicate/unwanted edges are produced
+  function checkIfEdgeExists(edges = [],id) {
+    return edges.findIndex(edge => edge.id === id) > -1
+  }
+
+  export function generateNodesAndEdges({obj, parent, nodes = [], edges = [], childCount = null}) {
+    console.log({obj})
     const values = Object.values(obj);
+    // console.log({values, obj})
     const hasNoChildren = values.every((value) => {
       return typeof value !== "object" || value === null;
     });
-    console.log({obj, hasNoChildren, parent})
-    const currentId = parent ? `${parent}-child` : "root";
+    let currentId = parent ? `${parent}-child` : "root";
+    if(childCount) currentId = `${currentId}-${childCount}`
     const isArray = Object.hasOwn(obj, 'length');
+    // const isArray = false;
     if(isArray) {
       obj.forEach((val, i) => {
-        console.log({val})
-        const edge = {
-          source: parent,
-          target: `${parent}-child-${i+1}`,
-          type: 'smoothstep'
-        }
-        edges.push(edge)
+        // primitive values in array
+        const nodeId = `${parent}-child-${i+1}`
         if(typeof val !== "object" || val === null) {
+          const edgeId = `${parent}->${parent}-child-${i+1}`
+          // if(!checkIfEdgeExists(edges, edgeId)) {
+            const edge = {
+              source: parent,
+              target: `${parent}-child-${i+1}`,
+              type: EDGE_TYPE,
+              // id: `${Math.random()}-x`
+              id: edgeId
+            }
+            edges.push(edge)
+          // }
           const node = {
-            id: `${parent}-child-${i+1}`,
+            id: nodeId ,
             data: {
               $primx: val
             },
-            type: 'customNode'
+            type: NODE_TYPE
           }
           nodes.push(node)
         } else {
-          // generateNodesAndEdges(val, parent, nodes, edges)
+          const isArray = Object.hasOwn(val, 'length');
+          // Array within the array
+          if(isArray) {
+            console.log(val, 'arr of arr')
+            const titleNode = {
+              type: NODE_TYPE,
+              data: {
+                title: 'array of array'
+              },
+              id: `${nodeId}-aoa`
+            }
+            nodes.push(titleNode);
+            const edge = {
+              target: `${nodeId}-aoa`,
+              source: parent,
+              type: EDGE_TYPE,
+              id: `${parent}-${nodeId}-aoa`
+            }
+            edges.push(edge);
+            generateNodesAndEdges({ obj: val, parent: `${nodeId}-aoa`, nodes, edges})
+          } else {
+            // Object within array
+            console.log('in else', val)
+            console.log({val, parent})
+            /* Since each element of array needs separate node and,
+             corresponding edge, I added the index as unique identifier
+             for each node. For object this is not required as the contents
+             of object will be contained in a single node  */
+            generateNodesAndEdges({
+              obj: val,
+              parent,
+              nodes,
+              edges,
+              childCount: i+1
+            })
+
+          }
         }
       })
     } else {
+      // RECURSION TERMINATING CONDITION!!
+      // goes on till a primitive value (leaf) is found
+      // in a traversal
       if (hasNoChildren) {
+        console.log('final one')
+        const nodeId = currentId;
+        const edgeId = `${parent}->${currentId}`
         const rootNode = {
-          id: currentId,
+          id: nodeId,
           data: { ...obj },
-          type: "customNode",
+          type: NODE_TYPE,
         };
         nodes.push(rootNode);
+        console.log({rootNode})
         if (!parent) {
           // nodes.push(rootNode)
-          return { nodes: [rootNode], edges: [] }
+          return;
+          // return { nodes: [rootNode], edges: [] }
         }
-        const edge = {
-          source: parent,
-          target: currentId,
-          type: "smoothstep",
-          // id: `${Math.random()}-x`
-          id:`${parent}->${currentId}`
-        };
-        edges.push(edge);
-        return { nodes, edges };
+        // if(!checkIfEdgeExists(edges, edgeId)) {
+          const edge = {
+            source: parent,
+            target: currentId,
+            type: EDGE_TYPE,
+            // id: `${Math.random()}-x`
+            id: edgeId
+          };
+          edges.push(edge);
+          // return { nodes, edges };
+        // }
+        return;
       }
       const { values: currentValues, children: currentChildren } =
         getPrimitives(obj);
-      // console.log({obj})
-      console.log({currentValues, currentChildren})
+      const others = currentChildren.reduce((acc,curr) => {
+        acc[curr] = Object.hasOwn(obj[curr],'length') ? 'Array' : 'Object'
+        return acc
+      },{})
       const primitivesNode = {
         id: currentId,
-        type: "customNode",
+        type: NODE_TYPE,
         data: {
           ...currentValues,
+          ...others
         },
       };
       nodes.push(primitivesNode);
-      currentChildren.forEach((key) => {
-        console.log({key})
+      currentChildren.forEach((key, i) => {
+        console.log({ob: obj[key], key})
+        const isArray = Object.hasOwn(obj[key], 'length');
+        const anchorNodeId = `${currentId}-${key}-${i+1}`;
           const childObjRootNode = {
-            id: `${currentId}-${key}`,
-            data: { title: key },
-            type: "customNode",
+            id: anchorNodeId,
+            data: { title: `${key}-${isArray ? 'array' : 'object'}` },
+            type: NODE_TYPE,
           };
           nodes.push(childObjRootNode);
           let parentToChildObjTitle = {}
           if(parent) {
             parentToChildObjTitle = {
-              target: `${currentId}-${key}`,
+              target: anchorNodeId,
               source: parent,
-              type: 'smoothstep',
-              id: `${parent}->${currentId}-${key}`
+              type: EDGE_TYPE,
+              id: `${parent}->${anchorNodeId}`
               // id: `${Math.random()}-x`
             }
             edges.push(parentToChildObjTitle)
           }
         const edgeSource = parent || "root";
-        const edgeTarget = parent ? currentId : `${currentId}-${key}`
-        // const edgeId = parent ? `${parent}->${currentId}` : `root->${currentId}-${key}`
-        const parentToPrimitiveChildren = {
-          type: "smoothstep",
-          source: edgeSource,
-          target: edgeTarget,
-          id: `${edgeSource}->${edgeTarget}`
-          // id: `${Math.random()}-x`
-        };
-        edges.push(parentToPrimitiveChildren);
-        generateNodesAndEdges(obj[key], `${currentId}-${key}`, nodes, edges);
+        const edgeTarget = parent ? currentId : anchorNodeId
+        // if(!checkIfEdgeExists(edges, `${edgeSource}->${edgeTarget}`)) {
+          const parentToPrimitiveChildren = {
+            type: EDGE_TYPE,
+            source: edgeSource,
+            target: edgeTarget,
+            id: `${edgeSource}->${edgeTarget}`
+            // id: `${Math.random()}-x`
+          };
+          edges.push(parentToPrimitiveChildren);
+        // }
+        // generateNodesAndEdges(obj[key], anchorNodeId, nodes, edges);
+        generateNodesAndEdges({ obj: obj[key], nodes, edges, parent: anchorNodeId})
       });
     }
     return { nodes, edges };
